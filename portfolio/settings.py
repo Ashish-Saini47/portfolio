62216203142a637a16687a3d90e695d8e6ff040e
Ashish-Saini47 <ashish.m.saini999@gmail.com>
2020-09-20 03:42:25 -0700

    added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,4 +132,6 @@ STATICFILES_DIRS = [
 ]
 #################################
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root') 
 #############################
